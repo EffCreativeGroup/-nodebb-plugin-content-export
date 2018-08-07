b4c915f8aa7b540afa1f24849bf8662be9a223fa
Ivan Brezhnev <bia@effcreative.com>
2018-08-07 11:17:42 +0300

    Fix topics recording

--- library.js
+++ library.js
@@ -5,6 +5,9 @@ const meta = module.parent.require('./meta');
 const winston = module.parent.require('winston');
 const nconf = module.parent.require('nconf');
 const _ = module.parent.require('underscore');
+const user = module.parent.require('./user');
+const topics = module.parent.require('./topics');
+const async = require('async');
 
 let library = {
   restApi: null,
@@ -82,8 +85,31 @@ library.reloadSettings = function () {
 
 library.savePostHook = function (req, next) {
   if (contentExportServiceConnector) {
-    const message = contentExportServiceConnector.storeMessage(req);
-    contentExportServiceConnector.sentToContentExportService(message);
+    const mesObj = {};
+    async.waterfall([
+      function (next) {
+        user.getUserFields(req.post.uid, ['username'], next);
+      },
+      function (userdata, next) {
+        mesObj.user = !userdata ? 'Empty name' : `${userdata.username}(${req.post.uid})`;
+        topics.getTopicsByTids([req.post.tid], req.post.uid, next);
+      },
+      function (topicdata, next) {
+        if (req.data.title) {
+          mesObj.topic = `Created topic: ${req.data.title}(${req.post.tid})`
+        } else {
+          mesObj.topic = 'Added comment to the topic: '
+          + (!topicdata.length ? 'Empty topic' : `${topicdata[0].title}(${req.post.tid})`);
+        }
+        next(null, `${mesObj.topic}; by user: ${mesObj.user}; with content: ${req.data.content}`)
+      },
+      function (content, next) {
+        req.data.content  = content;
+        const message = contentExportServiceConnector.storeMessage(req);
+        contentExportServiceConnector.sentToContentExportService(message);
+      }
+    ], next);
+
   }
   next(null, req);
 };
